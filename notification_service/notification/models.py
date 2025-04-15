af603d312b7b1e6563bdbb3df7a6d45424e914b3
Amal Ambrose <amalambrose555@gmail.com>
2025-04-15 13:45:37 +0530

    model and migrations update

--- notification_service/notification/models.py
+++ notification_service/notification/models.py
@@ -3,7 +3,7 @@ import uuid
 from django.db import models
 
 class Notification(models.Model):
-    notification_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user_id = models.UUIDField()  # Refers to user from accounts_service
     source_user_id = models.UUIDField()  # Refers to user from accounts_service
     type = models.CharField(
