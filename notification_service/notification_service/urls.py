d72ce7ee80116d2f558fd11636b43895eea240da
Amal Ambrose <amalambrose555@gmail.com>
2025-04-15 12:44:41 +0530

    url base update

--- notification_service/notification_service/urls.py
+++ notification_service/notification_service/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('notification.urls'))
+    path('api/notification_service/', include('notification.urls'))
 ]
