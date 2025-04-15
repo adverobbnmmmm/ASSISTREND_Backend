6767fd25c0f2f1d0c2c35fbd84d58f9e59c549bf
Amal Ambrose <amalambrose555@gmail.com>
2025-04-15 13:23:03 +0530

    url base updated

--- challenge_service/challenge_service/urls.py
+++ challenge_service/challenge_service/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/challenge_service/', include('challenge.urls')),
+    path('api/challenge-service/', include('challenge.urls')),
 ]
