0bda7adfc54e43699c857bb6dbb628d6b0c91947
Amal Ambrose <amalambrose555@gmail.com>
2025-04-15 13:21:02 +0530

    url base update

--- social_service/social_service/urls.py
+++ social_service/social_service/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/social_service/', include('social.urls')),
+    path('api/social-service/', include('social.urls')),
 ]
