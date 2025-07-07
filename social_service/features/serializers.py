d4f89362e33a09070d8f58c3975e69823c280992
jenjose72 <jenjose72@gmail.com>
2025-07-07 18:51:59 +0530

    bacek

--- social_service/features/serializers.py
+++ social_service/features/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.name', read_only=True)
+    
     class Meta:
         model = Post
-        fields = ['id', 'user', 'caption', 'image_url', 'category', 'created_at']
+        fields = ['id', 'user', 'username', 'caption', 'image_url', 'category', 'created_at']
